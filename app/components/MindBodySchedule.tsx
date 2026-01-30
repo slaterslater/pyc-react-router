d958f1321d46677e26339dbd071b0ead2621cef1
slaterslater <slaterslater@gmail.com>
2026-01-30 14:42:50 -0500

    adds schedule bg

--- app/components/MindBodySchedule.tsx
+++ app/components/MindBodySchedule.tsx
@@ -4,6 +4,6 @@ export function MindBodySchedule() {
   useMindBodyWidget()
 
   return (
-    <div className="mindbody-widget w-full outline-none mt-5" data-widget-type="Schedules" data-widget-id="254432542c3"></div>
+    <div className="mindbody-widget w-full outline-none mt-5 bg-[#FAF9F7] rounded-md min-h-[500px] flex flex-col items-center justify-center" data-widget-type="Schedules" data-widget-id="254432542c3"></div>
   )
 }
