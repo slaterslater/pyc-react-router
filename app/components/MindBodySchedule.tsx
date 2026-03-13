b0efda91f91c824602423d7717b1e35d08f143ad
slaterslater <slaterslater@gmail.com>
2026-03-13 15:16:40 -0400

    adds amenities placeholder

--- app/components/MindBodySchedule.tsx
+++ app/components/MindBodySchedule.tsx
@@ -1,9 +1,13 @@
 import { useMindBodyWidget } from "~/hooks/useMindBody"
 
-export function MindBodySchedule() {
+export function MindBodySchedule({ scheduleId }: { scheduleId: string }) {
   useMindBodyWidget()
 
   return (
-    <div className="mindbody-widget w-full outline-none mt-5 bg-[#FAF9F7] rounded-md min-h-[500px] flex flex-col items-center justify-center" data-widget-type="Schedules" data-widget-id="254432542c3"></div>
+    <div
+      className="mindbody-widget w-full outline-none mt-5 bg-[#FAF9F7] rounded-md min-h-[500px] flex flex-col items-center justify-center"
+      data-widget-type="Schedules"
+      data-widget-id={scheduleId}
+    />
   )
 }
--- app/routes/studios.$studio.tsx
+++ app/routes/studios.$studio.tsx
@@ -1,5 +1,6 @@
-import type { LoaderFunctionArgs } from "react-router";
+import { Link, useLoaderData, type LoaderFunctionArgs } from "react-router";
 import { Hero } from "~/components/Hero";
+import { MindBodySchedule } from "~/components/MindBodySchedule";
 import { PageLayout } from "~/components/PageLayout";
 
 export function meta() {
@@ -12,13 +13,32 @@ export function meta() {
 export function loader({ params }: LoaderFunctionArgs) {
   return {
     title: params.studio,
+    scheduleId: "254432542c3",
   }
 }
 
 export default function StudioRoute() {
+  const { scheduleId } = useLoaderData<typeof loader>()
+
   return (
     <PageLayout>
       <Hero title="main banner or video" />
+      <div className="flex gap-6 justify-around w-full p-8">
+        <Link to="/" className="btn-black flex-1">schedule</Link>
+        <Link to="/" className="btn-black flex-1">workshops</Link>
+        <Link to="/" className="btn-black flex-1">pricing</Link>
+        <Link to="/" className="btn-black flex-1">about the studio</Link>
+      </div>
+      <p className="mx-auto text-center text-light-gray max-w-5xl leading-relaxed text-2xl font-medium px-4">Power Yoga Collective is your home for hot power yoga. Our locally operated studios deliver heated classes that build strength, flexibility, and resilience, while fostering connection, consistency, and community across every location.</p>
+      <MindBodySchedule scheduleId={scheduleId} />
+      <h2 className="uppercase text-4xl font-medium mx-auto px-4 py-8">studio amenities</h2>
+      <div className="flex flex-wrap gap-8 px-8 py-4 w-full justify-center md:justify-around">
+        <img src="/development/towels.svg" alt="towels" width={135} />
+        <img src="/development/hot-yoga.svg" alt="hot yoga" width={135} />
+        <img src="/development/showers.svg" alt="showers" width={135} />
+        <img src="/development/power-yoga.svg" alt="warm yoga" width={135} />
+        <img src="/development/mat-rental.svg" alt="mat rental" width={135} />
+      </div>
     </PageLayout>
   )
 }
