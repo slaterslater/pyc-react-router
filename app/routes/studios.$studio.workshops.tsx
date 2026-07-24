13f5c5e002732b3b2a320e85f19d5d358d9fec5e
slaterslater <slaterslater@gmail.com>
2026-07-24 12:10:58 -0400

    guards against null offernigs and workshops

--- app/routes/studios.$studio.workshops.tsx
+++ app/routes/studios.$studio.workshops.tsx
@@ -6,24 +6,23 @@ import { payload } from "~/lib/payloadClient.server";
 export async function loader({ params }: LoaderFunctionArgs) {
   const { studio } = params;
   const data = await payload.request(STUDIO_WORKSHOP_QUERY, { studio })
-  const workshops = data.Studios.docs[0].workshops.docs[0].workshopBlocks;
-  const offerings = data.Studios.docs[0].offerings.docs[0].offeringBlocks;
+  const workshops = data.Studios.docs[0].workshops.docs[0]?.workshopBlocks;
+  const offerings = data.Studios.docs[0].offerings.docs[0]?.offeringBlocks;
   return { workshops, offerings };
 }
 
 export default function StudioWorkshops() {
   const { workshops, offerings } = useLoaderData<typeof loader>();
-  // console.log({ workshops, offerings })
   return (
     <>
       <ComingSoon title="Workshops" />
       <h2 className="heading text-center">Workshops We Offer</h2>
       <section className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4">
-        {workshops.map((workshop: Workshop) => <Workshop key={workshop.id} workshop={workshop} />)}
+        {workshops?.map((workshop: Workshop) => <Workshop key={workshop.id} workshop={workshop} />)}
       </section>
       <h2 className="heading text-center">Classes We Offer</h2>
       <section className="grid grid-cols-1 md:grid-cols-2 gap-4 px-4">
-        {offerings.map((offering: Offering) => <Offering key={offering.id} offering={offering} />)}
+        {offerings?.map((offering: Offering) => <Offering key={offering.id} offering={offering} />)}
       </section>
     </>
   )
