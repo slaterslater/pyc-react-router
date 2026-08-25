adbda714c8b81c75607cc95556160e332acd13b3
slaterslater <slaterslater@gmail.com>
2026-08-25 15:40:41 -0400

    adds video to feature

--- app/components/Feature.tsx
+++ app/components/Feature.tsx
@@ -1,17 +1,32 @@
 export function HomepageFeature({ feature }: { feature: HomepageFeatureType }) {
   return (
     <div>
-      <img src={feature.media?.sizes.tablet.url ?? feature.media?.url} alt={feature.media?.alt} className="w-full h-[350px] object-cover rounded-md" />
+      <FeatureMedia media={feature.media} />
       <h3 className="uppercase text-lg font-medium pt-3">{feature.title}</h3>
       <p className="text-light-gray pb-5">{feature.description}</p>
     </div>
   )
 }
 
+function FeatureMedia({ media }: { media: HomepageFeatureType['media'] }) {
+  const mimeType = media?.mimeType.split('/')[0];
+  switch (mimeType) {
+    case 'video':
+      return (
+        <video src={media?.url} autoPlay muted loop className="w-full h-[350px] object-cover rounded-md" />
+      );
+    case 'image':
+      return <img src={media?.sizes.tablet.url ?? media?.url} alt={media?.alt} className="w-full h-[350px] object-cover rounded-md" />;
+    default:
+      return <div className="w-full h-[350px] bg-charcoal rounded-md" />;
+  }
+}
+
 type HomepageFeatureType = {
   title: string;
   description: string;
   media: {
+    mimeType: string;
     url: string;
     alt: string;
     sizes: {
