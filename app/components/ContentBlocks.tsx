779e854a8624f82d1287f4622499b2131b96bf3a
slaterslater <slaterslater@gmail.com>
2026-08-28 09:33:30 -0400

    adds button into page banners

--- app/components/ContentBlocks.tsx
+++ app/components/ContentBlocks.tsx
@@ -48,7 +48,7 @@ export function ContentBlocks({ block }: { block: any }) {
       return <RichText data={block.richText} className={className} />
 
     case 'banner':
-      return <Hero hero={{ title: block.title, media: block.media }} parallax={false} />
+      return <Hero hero={block} parallax={false} />
 
     case 'reviews':
       const cols = Math.min(block.reviews.length, 3)
